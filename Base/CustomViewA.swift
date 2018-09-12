32307b3460867039956be3b6bbde04d6065a6d6e
wurunfa <runfa.wu@ubtrobot.com>
2018-09-12 14:28:21 +0800

    添加了base和category代码

---
+++ Base/CustomViewA.swift
@@ -0,0 +1,21 @@
+//
+//  CustomViewA.swift
+//  swiftDemo
+//
+//  Created by Fangfang Zhang on 2018/5/26.
+//  Copyright © 2018年 lisa. All rights reserved.
+//
+
+import UIKit
+
+class CustomViewA: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
